10241214bbc5ba655c3aab765a5ff0e5ac074737
ST10350498 <st10350498@myemeris.edu.za>
2026-09-11 18:00:34 +0200

    chore: Initial commit with project scaffolding and MenuItem model

---
+++ CoffeeNChill.Functions/Models/MenuItem.cs
@@ -0,0 +1,23 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace CoffeeNChill.Functions.Models
+{
+    /// <summary>
+    /// Represents a menu item stored in Azure Table Storage.
+    /// PartitionKey: Category (e.g., "Hot Drinks", "Cold Drinks", "Pastries", "Sandwiches")
+    /// RowKey: Unique SKU/ID (e.g., "COF-001", "PAS-104")
+    /// </summary>
+    public class MenuItem : ITableEntity
+    {
+        public string PartitionKey { get; set; } = "";
+        public string RowKey { get; set; } = "";
+        public ETag ETag { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public double Price { get; set; }
+        public bool IsAvailable { get; set; } = true;
+    }
+}
