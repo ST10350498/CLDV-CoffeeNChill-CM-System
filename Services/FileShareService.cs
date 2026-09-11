93a7b3edf16caf682fb7e2906dde228fd250d8b9
ST10350498 <st10350498@myemeris.edu.za>
2026-09-11 19:03:20 +0200

    feat: Enhance FileShareService with subdirectory support
    - Add InitializeAsync for staff-docs share
    
    - Add UploadFileAsync with ContentType header
    
    - Add ListFilesAsync with FileMetadata
    
    - Add DownloadFileAsync with FileNotFoundException handling

--- Services/FileShareService.cs
+++ Services/FileShareService.cs
@@ -1,60 +1,95 @@
 ﻿using Azure.Storage.Files.Shares;
 using Azure.Storage.Files.Shares.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace CoffeeNChill.Functions.Services
 {
     public class FileShareService
     {
         private readonly ShareClient _shareClient;
+        private readonly string _shareName;
 
         public FileShareService(string connectionString, string shareName = "staff-docs")
         {
-            var shareServiceClient = new ShareServiceClient(connectionString);
-            _shareClient = shareServiceClient.GetShareClient(shareName);
-            _shareClient.CreateIfNotExists();
+            _shareName = shareName;
+            _shareClient = new ShareClient(connectionString, shareName);
         }
 
-        public async Task UploadFileAsync(Stream fileStream, string fileName)
+        public async Task InitializeAsync()
         {
-            var directoryClient = _shareClient.GetRootDirectoryClient();
-            var fileClient = directoryClient.GetFileClient(fileName);
-            await fileClient.CreateAsync(fileStream.Length);
-            await fileClient.UploadAsync(fileStream);
-        }
+            await _shareClient.CreateIfNotExistsAsync();
 
-        public async Task<List<ShareFileItem>> ListFilesAsync()
-        {
-            var files = new List<ShareFileItem>();
-            var directoryClient = _shareClient.GetRootDirectoryClient();
-
-            await foreach (var item in directoryClient.GetFilesAndDirectoriesAsync())
+            var directories = new[] { "Recipes", "Manuals", "Policies" };
+            foreach (var dir in directories)
             {
-                var fileClient = directoryClient.GetFileClient(item.Name);
-                var properties = await fileClient.GetPropertiesAsync();
+                var dirClient = _shareClient.GetDirectoryClient(dir);
+                await dirClient.CreateIfNotExistsAsync();
+            }
+        }
 
-                files.Add(new ShareFileItem
+        public async Task<Uri> UploadFileAsync(string directoryName, string fileName, Stream fileStream, string contentType)
+        {
+            var directory = _shareClient.GetDirectoryClient(directoryName);
+            await directory.CreateIfNotExistsAsync();
+
+            var file = directory.GetFileClient(fileName);
+            await file.CreateAsync(fileStream.Length, new ShareFileHttpHeaders
+            {
+                ContentType = contentType
+            });
+
+            fileStream.Position = 0;
+            await file.UploadAsync(fileStream);
+            return file.Uri;
+        }
+
+        public async Task<IEnumerable<FileMetadata>> ListFilesAsync(string directoryName)
+        {
+            var files = new List<FileMetadata>();
+            var directory = _shareClient.GetDirectoryClient(directoryName);
+
+            if (!await directory.ExistsAsync())
+                return files;
+
+            await foreach (var item in directory.GetFilesAndDirectoriesAsync())
+            {
+                if (!item.IsDirectory)
                 {
-                    Name = item.Name,
-                    Size = properties.Value.ContentLength,
-                    LastModified = properties.Value.LastModified
-                });
+                    var fileClient = directory.GetFileClient(item.Name);
+                    var props = await fileClient.GetPropertiesAsync();
+                    files.Add(new FileMetadata
+                    {
+                        Name = item.Name,
+                        Size = props.Value.ContentLength,
+                        LastModified = props.Value.LastModified.DateTime,
+                        ContentType = props.Value.ContentType
+                    });
+                }
             }
             return files;
         }
 
-        public async Task<Stream> DownloadFileAsync(string fileName)
+        public async Task<Stream> DownloadFileAsync(string directoryName, string fileName)
         {
-            var directoryClient = _shareClient.GetRootDirectoryClient();
-            var fileClient = directoryClient.GetFileClient(fileName);
-            var response = await fileClient.DownloadAsync();
-            return response.Value.Content;
+            var directory = _shareClient.GetDirectoryClient(directoryName);
+            var file = directory.GetFileClient(fileName);
+
+            if (!await file.ExistsAsync())
+                throw new FileNotFoundException($"File '{fileName}' not found in '{directoryName}'.");
+
+            var download = await file.DownloadAsync();
+            return download.Value.Content;
         }
     }
 
-    public class ShareFileItem
+    public class FileMetadata
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
         public long Size { get; set; }
-        public DateTimeOffset? LastModified { get; set; }
+        public DateTime LastModified { get; set; }
+        public string ContentType { get; set; } = "";
     }
 }
